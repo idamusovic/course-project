b7c66824116ed45339e8a1cff9508ba1d9def41c
Ida Musovic <idamusovic99@gmail.com>
2023-06-01 21:14:25 +0200

    Finishing touches - Services module

--- project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,4 +9,15 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailComponent {
   @Input() recipe: Recipe;
+
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(){
+
+  }
+
+
+  onAddToSL(){
+    this.recipeService.addIngredientsToSL(this.recipe.ingredients);
+  }
 }
