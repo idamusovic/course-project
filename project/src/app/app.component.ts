7b0378624ec6554e4dbe60265e9df528733377be
Ida Musovic <idamusovic99@gmail.com>
2023-06-04 23:37:52 +0200

    course project - observables

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -9,7 +9,4 @@ export class AppComponent {
   title = 'project';
   loadedFeature = 'recipe';
 
-  onNavigate(feature: string){
-    this.loadedFeature = feature;
-  }
 }
--- project/src/app/shopping-list/shopping-list.component.ts
+++ project/src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,8 +9,9 @@ import { ShoppingListService } from './shopping-list.service';
   styleUrls: ['./shopping-list.component.css']
 
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit, OnDestroy{
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private slService: ShoppingListService){
 
@@ -17,11 +19,15 @@ export class ShoppingListComponent {
 
   ngOnInit(){
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged.subscribe(
+    this.subscription = this.slService.ingredientsChanged.subscribe(
       (ingredients:Ingredient[]) => {
         this.ingredients = ingredients;
       }
     )
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
